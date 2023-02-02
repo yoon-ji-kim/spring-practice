b4b7063e97f89f956b3a3413328806940c0932a4
yoon-ji-kim <yoonjigim0.0@gmail.com>
2023-02-02 11:28:04 +0900

    aoptest #1

---
+++ aoptest/src/main/java/com/douzone/aoptest/main/MainApp.java
@@ -0,0 +1,19 @@
+package com.douzone.aoptest.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		//컨테이너생성
+		ApplicationContext ac = new ClassPathXmlApplicationContext("config/applicationContext.xml");
+		
+		ProductService ps = ac.getBean(ProductService.class);
+		//메소드 부르기
+		ProductVo vo = ps.find("TV");
+		//화면 출력
+		System.out.println(vo);
+	}
+
+}
